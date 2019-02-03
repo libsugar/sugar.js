ffa474a1a63815b5f96ac9914515c31c7c7aef95
2A5F <Aditrc@outlook.com>
2019-02-03 22:56:25 +0800

    fix bug

--- dist/batchoper.js
+++ dist/batchoper.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
     typeof define === 'function' && define.amd ? define(['exports'], factory) :
-    (factory((global.uid = {})));
-}(this, (function (exports) { 'use strict';
+    (global = global || self, factory(global.uid = {}));
+}(this, function (exports) { 'use strict';
 
     function eqOr(self, ...others) {
         if (others.length == 0)
@@ -313,4 +313,4 @@
 
     Object.defineProperty(exports, '__esModule', { value: true });
 
-})));
+}));
