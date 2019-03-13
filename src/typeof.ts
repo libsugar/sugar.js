d8bae90bb7ed35a2f378e57ff526e336e36eb5d6
2A5F <Aditrc@outlook.com>
2019-03-13 23:09:14 +0800

    typeof

--- src/typeof.ts
+++ src/typeof.ts
@@ -2,7 +2,7 @@
 export type TypeString = 'bigint' | 'number' | 'string' | 'boolean' | 'symbol' | 'function' | 'object' | 'undefined'
 /** typeofOr
  *
- * 等价于 `typoef a === 'bigint' || typeof b === 'bigint' ...`
+ * 等价于 `typeof obj === 'bigint'`
  *
  * 判断一堆对象是否至少有一个是 `bigint`
  * @param type 目标类型
@@ -11,7 +11,7 @@ export type TypeString = 'bigint' | 'number' | 'string' | 'boolean' | 'symbol' |
 export function typeofOr(type: 'bigint', obj: any): obj is bigint
 /** typeofOr
  *
- * 等价于 `typoef a === 'bigint' || typeof b === 'bigint' ...`
+ * 等价于 `typeof obj === 'bigint'`
  *
  * 判断一堆对象是否至少有一个是 `bigint`
  * @param type 目标类型
@@ -21,7 +21,7 @@ export function typeofOr(type: 'bigint', obj: any): obj is bigint
 export function typeofOr(type: 'bigint', obj: any, ...objs: any[]): boolean
 /** typeofOr
  *
- * 等价于 `typoef a === 'number' || typeof b === 'number' ...`
+ * 等价于 `typeof obj === 'number'`
  *
  * 判断一堆对象是否至少有一个是 `number`
  * @param type 目标类型
@@ -30,7 +30,7 @@ export function typeofOr(type: 'bigint', obj: any, ...objs: any[]): boolean
 export function typeofOr(type: 'number', obj: any): obj is number
 /** typeofOr
  *
- * 等价于 `typoef a === 'number' || typeof b === 'number' ...`
+ * 等价于 `typeof obj === 'number'`
  *
  * 判断一堆对象是否至少有一个是 `number`
  * @param type 目标类型
@@ -40,7 +40,7 @@ export function typeofOr(type: 'number', obj: any): obj is number
 export function typeofOr(type: 'number', obj: any, ...objs: any[]): boolean
 /** typeofOr
  *
- * 等价于 `typoef a === 'string' || typeof b === 'string' ...`
+ * 等价于 `typeof obj === 'string'`
  *
  * 判断一堆对象是否至少有一个是 `string`
  * @param type 目标类型
@@ -49,7 +49,7 @@ export function typeofOr(type: 'number', obj: any, ...objs: any[]): boolean
 export function typeofOr(type: 'string', obj: any): obj is string
 /** typeofOr
  *
- * 等价于 `typoef a === 'string' || typeof b === 'string' ...`
+ * 等价于 `typeof obj === 'string'`
  *
  * 判断一堆对象是否至少有一个是 `string`
  * @param type 目标类型
@@ -59,7 +59,7 @@ export function typeofOr(type: 'string', obj: any): obj is string
 export function typeofOr(type: 'string', obj: any, ...objs: any[]): boolean
 /** typeofOr
  *
- * 等价于 `typoef a === 'boolean' || typeof b === 'boolean' ...`
+ * 等价于 `typeof obj === 'boolean'`
  *
  * 判断一堆对象是否至少有一个是 `boolean`
  * @param type 目标类型
@@ -68,7 +68,7 @@ export function typeofOr(type: 'string', obj: any, ...objs: any[]): boolean
 export function typeofOr(type: 'boolean', obj: any): obj is boolean
 /** typeofOr
  *
- * 等价于 `typoef a === 'boolean' || typeof b === 'boolean' ...`
+ * 等价于 `typeof obj === 'boolean'`
  *
  * 判断一堆对象是否至少有一个是 `boolean`
  * @param type 目标类型
@@ -78,7 +78,7 @@ export function typeofOr(type: 'boolean', obj: any): obj is boolean
 export function typeofOr(type: 'boolean', obj: any, ...objs: any[]): boolean
 /** typeofOr
  *
- * 等价于 `typoef a === 'symbol' || typeof b === 'symbol' ...`
+ * 等价于 `typeof obj === 'symbol'`
  *
  * 判断一堆对象是否至少有一个是 `symbol`
  * @param type 目标类型
@@ -87,7 +87,7 @@ export function typeofOr(type: 'boolean', obj: any, ...objs: any[]): boolean
 export function typeofOr(type: 'symbol', obj: any): obj is symbol
 /** typeofOr
  *
- * 等价于 `typoef a === 'symbol' || typeof b === 'symbol' ...`
+ * 等价于 `typeof obj === 'symbol'`
  *
  * 判断一堆对象是否至少有一个是 `symbol`
  * @param type 目标类型
@@ -97,7 +97,7 @@ export function typeofOr(type: 'symbol', obj: any): obj is symbol
 export function typeofOr(type: 'symbol', obj: any, ...objs: any[]): boolean
 /** typeofOr
  *
- * 等价于 `typoef a === 'object' || typeof b === 'object' ...`
+ * 等价于 `typeof obj === 'object'`
  *
  * 判断一堆对象是否至少有一个是 `object`
  * @param type 目标类型
@@ -106,7 +106,7 @@ export function typeofOr(type: 'symbol', obj: any, ...objs: any[]): boolean
 export function typeofOr(type: 'object', obj: any): obj is object
 /** typeofOr
  *
- * 等价于 `typoef a === 'object' || typeof b === 'object' ...`
+ * 等价于 `typeof obj === 'object'`
  *
  * 判断一堆对象是否至少有一个是 `object`
  * @param type 目标类型
@@ -116,7 +116,7 @@ export function typeofOr(type: 'object', obj: any): obj is object
 export function typeofOr(type: 'object', obj: any, ...objs: any[]): boolean
 /** typeofOr
  *
- * 等价于 `typoef a === 'undefined' || typeof b === 'undefined' ...`
+ * 等价于 `typeof obj === 'undefined'`
  *
  * 判断一堆对象是否至少有一个是 `undefined`
  * @param type 目标类型
@@ -125,7 +125,7 @@ export function typeofOr(type: 'object', obj: any, ...objs: any[]): boolean
 export function typeofOr(type: 'undefined', obj: any): obj is undefined
 /** typeofOr
  *
- * 等价于 `typoef a === 'undefined' || typeof b === 'undefined' ...`
+ * 等价于 `typeof obj === 'undefined'`
  *
  * 判断一堆对象是否至少有一个是 `undefined`
  * @param type 目标类型
@@ -135,7 +135,7 @@ export function typeofOr(type: 'undefined', obj: any): obj is undefined
 export function typeofOr(type: 'undefined', obj: any, ...objs: any[]): boolean
 /** typeofOr
  *
- * 等价于 `typoef a === 'function' || typeof b === 'function' ...`
+ * 等价于 `typeof obj === 'function'`
  *
  * 判断一堆对象是否至少有一个是 `function`
  * @param type 目标类型
@@ -144,7 +144,7 @@ export function typeofOr(type: 'undefined', obj: any, ...objs: any[]): boolean
 export function typeofOr(type: 'function', obj: any): obj is Function
 /** typeofOr
  *
- * 等价于 `typoef a === 'function' || typeof b === 'function' ...`
+ * 等价于 `typeof obj === 'function'`
  *
  * 判断一堆对象是否至少有一个是 `function`
  * @param type 目标类型
@@ -154,7 +154,7 @@ export function typeofOr(type: 'function', obj: any): obj is Function
 export function typeofOr(type: 'function', obj: any, ...objs: any[]): boolean
 /** typeofOr
  *
- * 等价于 `typoef a === type || typeof b === type ...`
+ * 等价于 `typeof obj === type`
  *
  * 判断一堆对象是否至少有一个是某个类型
  * @param type 目标类型
@@ -163,7 +163,7 @@ export function typeofOr(type: 'function', obj: any, ...objs: any[]): boolean
 export function typeofOr(type: TypeString, obj: any): boolean
 /** typeofOr
  * 
- * 等价于 `typoef a === type || typeof b === type ...`
+ * 等价于 `typeof a === type || typeof b === type ...`
  * 
  * 判断一堆对象是否至少有一个是某个类型
  * @param type 目标类型
@@ -180,7 +180,7 @@ export function typeofOr(type: TypeString, ...objs: any[]) {
 }
 /** typeofAnd
  *
- * 等价于 `typoef a === 'bigint' && typeof b === 'bigint' ...`
+ * 等价于 `typeof obj === 'bigint' && typeof b === 'bigint' ...`
  *
  * 判断一堆对象是否全部是 `bigint`
  * @param type 目标类型
@@ -189,7 +189,7 @@ export function typeofOr(type: TypeString, ...objs: any[]) {
 export function typeofAnd(type: 'bigint', obj: any): obj is bigint
 /** typeofAnd
  *
- * 等价于 `typoef a === 'bigint' && typeof b === 'bigint' ...`
+ * 等价于 `typeof obj === 'bigint' && typeof b === 'bigint' ...`
  *
  * 判断一堆对象是否至全部是 `bigint`
  * @param type 目标类型
@@ -199,7 +199,7 @@ export function typeofAnd(type: 'bigint', obj: any): obj is bigint
 export function typeofAnd(type: 'bigint', ...objs: any[]): boolean
 /** typeofAnd
  *
- * 等价于 `typoef a === 'number' && typeof b === 'number' ...`
+ * 等价于 `typeof obj === 'number' && typeof b === 'number' ...`
  *
  * 判断一堆对象是否全部是 `number`
  * @param type 目标类型
@@ -208,7 +208,7 @@ export function typeofAnd(type: 'bigint', ...objs: any[]): boolean
 export function typeofAnd(type: 'number', obj: any): obj is number
 /** typeofAnd
  *
- * 等价于 `typoef a === 'number' && typeof b === 'number' ...`
+ * 等价于 `typeof obj === 'number' && typeof b === 'number' ...`
  *
  * 判断一堆对象是否至全部是 `number`
  * @param type 目标类型
@@ -218,7 +218,7 @@ export function typeofAnd(type: 'number', obj: any): obj is number
 export function typeofAnd(type: 'number', ...objs: any[]): boolean
 /** typeofAnd
  *
- * 等价于 `typoef a === 'string' && typeof b === 'string' ...`
+ * 等价于 `typeof obj === 'string' && typeof b === 'string' ...`
  *
  * 判断一堆对象是否全部是 `string`
  * @param type 目标类型
@@ -227,7 +227,7 @@ export function typeofAnd(type: 'number', ...objs: any[]): boolean
 export function typeofAnd(type: 'string', obj: any): obj is string
 /** typeofAnd
  *
- * 等价于 `typoef a === 'string' && typeof b === 'string' ...`
+ * 等价于 `typeof obj === 'string' && typeof b === 'string' ...`
  *
  * 判断一堆对象是否全部是 `string`
  * @param type 目标类型
@@ -237,7 +237,7 @@ export function typeofAnd(type: 'string', obj: any): obj is string
 export function typeofAnd(type: 'string', ...objs: any[]): boolean
 /** typeofAnd
  *
- * 等价于 `typoef a === 'boolean' && typeof b === 'boolean' ...`
+ * 等价于 `typeof obj === 'boolean' && typeof b === 'boolean' ...`
  *
  * 判断一堆对象是否全部是 `boolean`
  * @param type 目标类型
@@ -246,7 +246,7 @@ export function typeofAnd(type: 'string', ...objs: any[]): boolean
 export function typeofAnd(type: 'boolean', obj: any): obj is boolean
 /** typeofAnd
  *
- * 等价于 `typoef a === 'boolean' && typeof b === 'boolean' ...`
+ * 等价于 `typeof obj === 'boolean' && typeof b === 'boolean' ...`
  *
  * 判断一堆对象是否全部是 `boolean`
  * @param type 目标类型
@@ -256,7 +256,7 @@ export function typeofAnd(type: 'boolean', obj: any): obj is boolean
 export function typeofAnd(type: 'boolean', ...objs: any[]): boolean
 /** typeofAnd
  *
- * 等价于 `typoef a === 'symbol' && typeof b === 'symbol' ...`
+ * 等价于 `typeof obj === 'symbol' && typeof b === 'symbol' ...`
  *
  * 判断一堆对象是否全部是 `symbol`
  * @param type 目标类型
@@ -265,7 +265,7 @@ export function typeofAnd(type: 'boolean', ...objs: any[]): boolean
 export function typeofAnd(type: 'symbol', obj: any): obj is symbol
 /** typeofAnd
  *
- * 等价于 `typoef a === 'symbol' && typeof b === 'symbol' ...`
+ * 等价于 `typeof obj === 'symbol' && typeof b === 'symbol' ...`
  *
  * 判断一堆对象是否全部是 `symbol`
  * @param type 目标类型
@@ -275,7 +275,7 @@ export function typeofAnd(type: 'symbol', obj: any): obj is symbol
 export function typeofAnd(type: 'symbol', ...objs: any[]): boolean
 /** typeofAnd
  *
- * 等价于 `typoef a === 'object' && typeof b === 'object' ...`
+ * 等价于 `typeof obj === 'object' && typeof b === 'object' ...`
  *
  * 判断一堆对象是否全部是 `object`
  * @param type 目标类型
@@ -284,7 +284,7 @@ export function typeofAnd(type: 'symbol', ...objs: any[]): boolean
 export function typeofAnd(type: 'object', obj: any): obj is object
 /** typeofAnd
  *
- * 等价于 `typoef a === 'object' && typeof b === 'object' ...`
+ * 等价于 `typeof obj === 'object' && typeof b === 'object' ...`
  *
  * 判断一堆对象是否全部是 `object`
  * @param type 目标类型
@@ -294,7 +294,7 @@ export function typeofAnd(type: 'object', obj: any): obj is object
 export function typeofAnd(type: 'object', ...objs: any[]): boolean
 /** typeofAnd
  *
- * 等价于 `typoef a === 'undefined' && typeof b === 'undefined' ...`
+ * 等价于 `typeof obj === 'undefined' && typeof b === 'undefined' ...`
  *
  * 判断一堆对象是否全部是 `undefined`
  * @param type 目标类型
@@ -303,7 +303,7 @@ export function typeofAnd(type: 'object', ...objs: any[]): boolean
 export function typeofAnd(type: 'undefined', obj: any): obj is undefined
 /** typeofAnd
  *
- * 等价于 `typoef a === 'undefined' && typeof b === 'undefined' ...`
+ * 等价于 `typeof obj === 'undefined' && typeof b === 'undefined' ...`
  *
  * 判断一堆对象是否全部是 `undefined`
  * @param type 目标类型
@@ -313,7 +313,7 @@ export function typeofAnd(type: 'undefined', obj: any): obj is undefined
 export function typeofAnd(type: 'undefined', ...objs: any[]): boolean
 /** typeofAnd
  *
- * 等价于 `typoef a === 'function' && typeof b === 'function' ...`
+ * 等价于 `typeof obj === 'function' && typeof b === 'function' ...`
  *
  * 判断一堆对象是否全部是 `function`
  * @param type 目标类型
@@ -322,7 +322,7 @@ export function typeofAnd(type: 'undefined', ...objs: any[]): boolean
 export function typeofAnd(type: 'function', obj: any): obj is Function
 /** typeofAnd
  *
- * 等价于 `typoef a === 'function' && typeof b === 'function' ...`
+ * 等价于 `typeof obj === 'function' && typeof b === 'function' ...`
  *
  * 判断一堆对象是否全部是 `function`
  * @param type 目标类型
@@ -332,7 +332,7 @@ export function typeofAnd(type: 'function', obj: any): obj is Function
 export function typeofAnd(type: 'function', ...objs: any[]): boolean
 /** typeofAnd
  *
- * 等价于 `typoef a === type && typeof b === type ...`
+ * 等价于 `typeof obj === type`
  *
  * 判断一堆对象是否全部是某个类型
  * @param type 目标类型
@@ -341,7 +341,7 @@ export function typeofAnd(type: 'function', ...objs: any[]): boolean
 export function typeofAnd(type: TypeString, obj: any): boolean
 /** typeofAnd
  *
- * 等价于 `typoef a === type && typeof b === type ...`
+ * 等价于 `typeof a === type && typeof b === type ...`
  *
  * 判断一堆对象是否至全部是某个类型
  * @param type 目标类型
@@ -355,4 +355,103 @@ export function typeofAnd(type: TypeString, ...objs: any[]) {
         if (typeof o !== type) return false
     }
     return true
+}
+/** typeofAny
+ *
+ * 等价于 `typeof obj === 'bigint'`
+ *
+ * 判断一堆对象是否至少有一个是 `bigint`
+ * @param type 目标类型
+ * @param obj 要判断的对象
+ */
+export function typeofAny(obj: any, type: 'bigint'): obj is bigint
+/** typeofAny
+ *
+ * 等价于 `typeof obj === 'number'`
+ *
+ * 判断一堆对象是否至少有一个是 `number`
+ * @param type 目标类型
+ * @param obj 要判断的对象
+ */
+export function typeofAny(obj: any, type: 'number'): obj is number
+/** typeofAny
+ *
+ * 等价于 `typeof obj === 'string'`
+ *
+ * 判断一堆对象是否至少有一个是 `string`
+ * @param type 目标类型
+ * @param obj 要判断的对象
+ */
+export function typeofAny(obj: any, type: 'string'): obj is string
+/** typeofAny
+ *
+ * 等价于 `typeof obj === 'boolean'`
+ *
+ * 判断一堆对象是否至少有一个是 `boolean`
+ * @param type 目标类型
+ * @param obj 要判断的对象
+ */
+export function typeofAny(obj: any, type: 'boolean'): obj is boolean
+/** typeofAny
+ *
+ * 等价于 `typeof obj === 'symbol'`
+ *
+ * 判断一堆对象是否至少有一个是 `symbol`
+ * @param type 目标类型
+ * @param obj 要判断的对象
+ */
+export function typeofAny(obj: any, type: 'symbol'): obj is symbol
+/** typeofAny
+ *
+ * 等价于 `typeof obj === 'object'`
+ *
+ * 判断一堆对象是否至少有一个是 `object`
+ * @param type 目标类型
+ * @param obj 要判断的对象
+ */
+export function typeofAny(obj: any, type: 'object'): obj is object
+/** typeofAny
+ *
+ * 等价于 `typeof obj === 'undefined'`
+ *
+ * 判断一堆对象是否至少有一个是 `undefined`
+ * @param type 目标类型
+ * @param obj 要判断的对象
+ */
+export function typeofAny(obj: any, type: 'undefined'): obj is undefined
+/** typeofAny
+ *
+ * 等价于 `typeof obj === 'function'`
+ *
+ * 判断一堆对象是否至少有一个是 `function`
+ * @param type 目标类型
+ * @param obj 要判断的对象
+ */
+export function typeofAny(obj: any, type: 'function'): obj is Function
+/** typeofAny
+ *
+ * 等价于 `typeof obj === type `
+ *
+ * 判断一堆对象是否至少有一个是某个类型
+ * @param type 目标类型
+ * @param obj 要判断的对象
+ */
+export function typeofAny(any: any, type: TypeString): boolean
+/** typeofAny
+ * 
+ * 等价于 `typeof obj === a || typeof obj === b ...`
+ * 
+ * 判断一堆对象是否至少有一个是某个类型
+ * @param type 目标类型
+ * @param obj 要判断的对象
+ * @param objs 其他要判断的对象
+ */
+export function typeofAny(obj: any, type: TypeString, ...types: TypeString[]): boolean
+export function typeofAny(obj: any, ...types: TypeString[]) {
+    if (types.length == 0) return false
+    const type = typeof obj
+    for (const t of types) {
+        if (type === t) return true
+    }
+    return false
 }
