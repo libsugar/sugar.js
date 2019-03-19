79ea29adf3a109fb55d83040fd274f5daf517733
2A5F <Aditrc@outlook.com>
2019-03-19 20:43:02 +0800

    eq

--- src/eq.ts
+++ src/eq.ts
@@ -1,24 +1,24 @@
 /** eqOr
  * 
- * 等价于 `self == a || self == b ....`
- * @param self 比较目标
- * @returns 直接返回自身
+ * Equivalent to `self == a || self == b ....`
+ * @param self Compare target
+ * @returns Return directly to itself
  */
 export function eqOr<T>(self: T): T
 /** eqOr
  * 
- * 等价于 `self == a || self == b ....`
- * @param self 比较目标
- * @param Other 被比较的对象
- * @param others 其他被比较的对象
+ * Equivalent to `self == a || self == b ....`
+ * @param self Compare target
+ * @param Other The object being compared
+ * @param others Other compared objects
  */
 export function eqOr<T>(self: T, Other: T, ...others: T[]): boolean
 /** eqOr
  *
- * 等价于 `self == a || self == b ....`
- * @param self 比较目标
- * @param Other 被比较的对象
- * @param others 其他被比较的对象
+ * Equivalent to `self == a || self == b ....`
+ * @param self Compare target
+ * @param Other The object being compared
+ * @param others Other compared objects
  */
 export function eqOr<T>(self: T, Other: T, ...others: any[]): boolean
 export function eqOr(self: any, ...others: any[]) {
@@ -30,25 +30,25 @@ export function eqOr(self: any, ...others: any[]) {
 }
 /** eqAnd
  * 
- * 等价于 `self == a && self == b ....`
- * @param self 比较目标
- * @returns 直接返回自身
+ * Equivalent to `self == a && self == b ....`
+ * @param self Compare target
+ * @returns Return directly to itself
  */
 export function eqAnd<T>(self: T): T
 /** eqAnd
  * 
- * 等价于 `self == a && self == b ....`
- * @param self 比较目标
- * @param Other 被比较的对象
- * @param others 其他被比较的对象
+ * Equivalent to `self == a && self == b ....`
+ * @param self Compare target
+ * @param Other The object being compared
+ * @param others Other compared objects
  */
 export function eqAnd<T>(self: T, Other: T, ...others: T[]): boolean
 /** eqAnd
  *
- * 等价于 `self == a && self == b ....`
- * @param self 比较目标
- * @param Other 被比较的对象
- * @param others 其他被比较的对象
+ * Equivalent to `self == a && self == b ....`
+ * @param self Compare target
+ * @param Other The object being compared
+ * @param others Other compared objects
  */
 export function eqAnd<T>(self: T, Other: T, ...others: any[]): boolean
 export function eqAnd(self: any, ...others: any[]) {
@@ -60,25 +60,25 @@ export function eqAnd(self: any, ...others: any[]) {
 }
 /** fEqOr
  * 
- * 等价于 `self === a || self === b ....`
- * @param self 比较目标
- * @returns 直接返回自身
+ * Equivalent to `self === a || self === b ....`
+ * @param self Compare target
+ * @returns Return directly to itself
  */
 export function fEqOr<T>(self: T): T
 /** fEqOr
  * 
- * 等价于 `self === a || self === b ....`
- * @param self 比较目标
- * @param Other 被比较的对象
- * @param others 其他被比较的对象
+ * Equivalent to `self === a || self === b ....`
+ * @param self Compare target
+ * @param Other The object being compared
+ * @param others Other compared objects
  */
 export function fEqOr<T>(self: T, Other: T, ...others: T[]): boolean
 /** fEqOr
  *
- * 等价于 `self === a || self === b ....`
- * @param self 比较目标
- * @param Other 被比较的对象
- * @param others 其他被比较的对象
+ * Equivalent to `self === a || self === b ....`
+ * @param self Compare target
+ * @param Other The object being compared
+ * @param others Other compared objects
  */
 export function fEqOr<T>(self: T, Other: T, ...others: any[]): boolean
 export function fEqOr(self: any, ...others: any[]) {
@@ -90,25 +90,25 @@ export function fEqOr(self: any, ...others: any[]) {
 }
 /** fEqAnd
  * 
- * 等价于 `self === a && self === b ....`
- * @param self 比较目标
- * @returns 直接返回自身
+ * Equivalent to `self === a && self === b ....`
+ * @param self Compare target
+ * @returns Return directly to itself
  */
 export function fEqAnd<T>(self: T): T
 /** fEqAnd
  * 
- * 等价于 `self === a && self === b ....`
- * @param self 比较目标
- * @param Other 被比较的对象
- * @param others 其他被比较的对象
+ * Equivalent to `self === a && self === b ....`
+ * @param self Compare target
+ * @param Other The object being compared
+ * @param others Other compared objects
  */
 export function fEqAnd<T>(self: T, Other: T, ...others: T[]): boolean
 /** fEqAnd
  *
- * 等价于 `self === a && self === b ....`
- * @param self 比较目标
- * @param Other 被比较的对象
- * @param others 其他被比较的对象
+ * Equivalent to `self === a && self === b ....`
+ * @param self Compare target
+ * @param Other The object being compared
+ * @param others Other compared objects
  */
 export function fEqAnd<T>(self: T, Other: T, ...others: any[]): boolean
 export function fEqAnd(self: any, ...others: any[]) {
