73fff04f3ffb97824d6d00e456db85b47df02664
2a5f <u2a5f@outlook.com>
2020-12-21 12:30:22 +0800

    fn

--- src/fn.ts
+++ src/fn.ts
@@ -5,3 +5,17 @@ export function getDefault<T>(defv: DefaultOrFunc<T>): T {
     if (typeof defv === 'function') return (defv as any)()
     return defv
 }
+
+export function identity<T>(val: T): T {
+    return val
+}
+
+export function empty(): void { }
+
+export function TODO(reason?: any): never {
+    throw reason ?? 'todo'
+}
+
+export function raise(err: any): never {
+    throw err
+}
