03a088b5546c864e7683aadbbda276a1172424c0
2a5f <u2a5f@outlook.com>
2020-12-21 12:05:43 +0800

    types

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,7 @@ export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) ex
 
 export type LastOfUnion<T> = UnionToIntersection<T extends any ? () => T : never> extends () => (infer R) ? R : never
 
-type _UnionToTuple<T, L = LastOfUnion<T>, N = [T] extends [never] ? true : false> = true extends N ? [] : TuplePush<_UnionToTuple<Exclude<T, L>>, L>
+type _UnionToTuple<T, L = LastOfUnion<T>> = [L] extends [never] ? [] : [..._UnionToTuple<Exclude<T, L>>, L]
 export type UnionToTuple<T> = _UnionToTuple<T>
 
 export type TuplePush<T extends any[], V> = [...T, V]
@@ -14,10 +14,9 @@ export type TupleTail<T extends any[]> = T extends [any, ...infer R] ? R : never
 
 export type TupleFirst<T extends any[]> = T extends [infer F, ...any[]] ? F : never
 
-export type TupleLast<T extends any[]> = T extends [any, ...any[]] ? T[TupleTail<T>['length']] : never
+export type TupleLast<T extends any[]> = T[TupleTail<T>['length']]
 
-type _TupleBody<T extends any[], A extends any[] = []> = [...A, never]['length'] extends T['length'] ? A : _TupleBody<T, [...A, T[A['length']]]>
-export type TupleBody<T extends any[]> = _TupleBody<T>
+export type TupleBody<T extends any[]> = T extends [...infer B, ...[any]] ? B : never
 
 type _TupleN<T, N extends number, A extends any[] = []> = A['length'] extends N ? A : _TupleN<T, N, [...A, T]>
 export type TupleN<T, N extends number> = _TupleN<T, N>
