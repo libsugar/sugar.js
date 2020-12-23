a1621f79542f8154467b75f3373bfe7bc7db18d9
2a5f <u2a5f@outlook.com>
2020-12-23 14:43:40 +0800

    map test

--- src/map.test.ts
+++ src/map.test.ts
@@ -1,1 +1,22 @@
-import { } from './map'
+import { MutMapEx } from './map'
+
+test('getOrDefault', () => {
+    let map = MutMapEx(new Map)
+    expect(map.getOrDefault(1, 2)).toBe(2)
+    map.set(1, 3)
+    expect(map.getOrDefault(1, 2)).toBe(3)
+})
+
+test('tryGet', () => {
+    let map = MutMapEx(new Map)
+    expect(map.tryGet(1).has).toBeFalsy()
+    map.set(1, 3)
+    expect(map.tryGet(1).val).toBe(3)
+})
+
+test('getOrAdd', () => {
+    let map = MutMapEx(new Map)
+    expect(map.get(1)).toBeUndefined()
+    expect(map.getOrAdd(1, () => 2)).toBe(2)
+    expect(map.get(1)).toBe(2)
+})
