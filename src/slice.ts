76fa44a4fc3f4d195982522607585ce33d07a20e
2a5f <u2a5f@outlook.com>
2020-12-24 23:11:57 +0800

    slice

--- src/slice.ts
+++ src/slice.ts
@@ -1,14 +1,15 @@
+import { eqOr } from "./bop";
 import { Ranges, rangeAll } from "./range";
 
 export interface Span<A extends ArrayLike<any>> extends ArrayLike<A[number]> { }
 export class Span<A extends ArrayLike<any>> {
-    private readonly arr: A
-    private from: number
-    private to: number
+    readonly #arr: A
+    #from: number
+    #to: number
     private constructor(arr: A, from: number, to: number) {
-        this.arr = arr
-        this.from = from
-        this.to = to
+        this.#arr = arr
+        this.#from = from
+        this.#to = to
         return new Proxy(this, {
             has: (target, property) => {
                 if (typeof property === 'number') return target.has(property)
@@ -16,6 +17,8 @@ export class Span<A extends ArrayLike<any>> {
             },
             get: (target, property, receiver) => {
                 if (typeof property === 'number') return target.get(property)
+                if (eqOr(property, 'get', 'set', 'slice')) return this[(property as any)].bind(this)
+                if (eqOr(property, 'length')) return this[(property as any)]
                 return Reflect.get(target, property, receiver)
             },
             set: (target, property, value, receiver) => {
@@ -29,8 +32,7 @@ export class Span<A extends ArrayLike<any>> {
     static from<A extends ArrayLike<any>>(arr: A, range?: Ranges<number>): Span<A>
     static from<A extends ArrayLike<any>>(arr: A | Span<A>, range?: Ranges<number>): Span<A> {
         if (arr instanceof Span) {
-            let [from = 0, to = arr.to - arr.from] = Ranges.toTuple(range ?? rangeAll())
-            return new Span<A>(arr.arr, arr.from + from, arr.from + to)
+            return arr.slice(range)
         } else {
             let [from = 0, to = arr.length] = Ranges.toTuple(range ?? rangeAll())
             return new Span<A>(arr, from, to)
@@ -43,20 +45,21 @@ export class Span<A extends ArrayLike<any>> {
 
     get(i: number): A[number] {
         if (i >= this.length || i < 0) return
-        return this.arr[this.from + i]
+        return this.#arr[this.#from + i]
     }
     set(i: number, v: A[number]) {
         if (i >= this.length || i < 0) return false;
-        (this.arr as any)[this.from + i] = v
+        (this.#arr as any)[this.#from + i] = v
         return true
     }
 
     get length() {
-        return this.to - this.from
+        return this.#to - this.#from
     }
 
-    slice(range: Ranges<number>): Span<A> {
-        return Span.from(this, range)
+    slice(range?: Ranges<number>): Span<A> {
+        let [from = 0, to = this.length] = Ranges.toTuple(range ?? rangeAll())
+        return new Span<A>(this.#arr, this.#from + from, this.#from + to)
     }
 }
 
