6fa71eeba67601d2803cb10dc3f617201b893171
2A5F <u2a5f@outlook.com>
2021-02-03 22:04:03 +0800

    2.2

--- src/types.ts
+++ src/types.ts
@@ -83,6 +83,55 @@ type _TupleN<T, N extends number, A extends any[] = []> = A['length'] extends N 
  */
 export type TupleN<T, N extends number> = _TupleN<T, N>
 
+/**
+ * Make all properties in T writeable
+ * 
+ * ```ts
+ * Writeable<readonly [1, 2, 3]> => [1, 2, 3]
+ * ```
+ */
+export type Writeable<T> = { -readonly [P in keyof T]: T[P] }
+
+/**
+ * Make all properties in T writeable
+ * 
+ * ```ts
+ * DeepWriteable<readonly [readonly [1]]> => [[1]]
+ * ```
+ */
+export type DeepWriteable<T> = { -readonly [P in keyof T]: DeepWriteable<T[P]> }
+
+/**
+ * Make all properties in T readonly
+ * 
+ * ```ts
+ * DeepReadonly<[[1]]> => readonly [readonly [1]]
+ * ```
+ */
+export type DeepReadonly<T> = { readonly [P in keyof T]: DeepReadonly<T[P]> }
+
+/**
+ * Make all properties in T optional
+ * 
+ * ```ts
+ * DeepPartial<{ a: { b: 1 } }> => { a?: { b?: 1 } }
+ * ```
+ */
+export type DeepPartial<T> = { [P in keyof T]?: DeepPartial<T[P]> }
+
+/**
+ * Make all properties in T required
+ * 
+ * ```ts
+ * DeepRequired<{ a?: { b?: 1 } }> => { a: { b: 1 } }
+ * ```
+ */
+export type DeepRequired<T> = { [P in keyof T]-?: DeepRequired<T[P]> }
+
+/**
+ * If `A` does not extends `E`, return `never`
+ */
+export type AssertType<E, A> = A extends E ? A : never
 
 /** Like `Map<T>` */
 export interface MapLike<K, V> {
